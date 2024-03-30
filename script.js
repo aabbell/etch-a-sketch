e11201d685c389472de1c124458a9e8794d83d8f
aabbell <abelalemneh68@gmail.com>
2024-03-31 00:09:20 +0300

    add style.css and index.html and script.js

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ for (let i = 0; i < 16*16; i++){
     const grid = document.createElement("div");
     grid.classList.add('grid');
     container.appendChild(grid);
+    grid.addEventListener('mouseover',function(){
+        grid.style.backgroundColor = 'black';
+        
+    })
     }
+
+
+
+
