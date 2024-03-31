92681d169efdfb7b34d7f1583f87fcd84aad9897
aabbell <abelalemneh68@gmail.com>
2024-03-31 12:41:09 +0300

    add script.js

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+const body = document.querySelector(".body");
 const container = document.querySelector(".container");
-
-for (let i = 0; i < 16*16; i++){
-    const grid = document.createElement("div");
-    grid.classList.add('grid');
-    container.appendChild(grid);
-    grid.addEventListener('mouseover',function(){
-        grid.style.backgroundColor = 'black';
-        
-    })
+const button = document.createElement("button")
+button.textContent = ("addgrid");
+body.appendChild(button)
+button.addEventListener('click',function(){
+    let input = prompt("Enter a number for the grid");
+    while(input >= 100){
+        let input = prompt("Enter a number for the grid");
     }
+    for (let i = 0; i < input * input; i++){
+        const grid = document.createElement("div");
+        grid.classList.add('grid');
+        container.appendChild(grid);
+        grid.addEventListener('mouseover',function(){
+            grid.style.backgroundColor = 'black';
+            
+        })
+        }
+    
+})
+
+
+
+
 
 
 
